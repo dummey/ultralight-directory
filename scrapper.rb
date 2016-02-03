35db6e25c83ac669229e7defb717875f647ff9ce
Ricky Ng <dummey@gmail.com>
2016-02-03 13:42:34 -0600

    Well formatted csv.

--- scrapper.rb
+++ scrapper.rb
@@ -4,10 +4,12 @@ require 'csv'
 def get_relevant_info(url)
   doc = Pismo::Document.new(url)
 
-  [doc.description.strip, doc.doc.xpath("//title").children.text.strip, doc.title.strip].each do |i|
+  [doc.description.strip, doc.doc.xpath("//title").children.text.strip, doc.title.strip, "na"].each do |i|
     return i unless i.empty?
   end
 end
+
+rows = []
 
 File.open("source/directory", "r") do |f|
   f.each_line do |url|
@@ -16,15 +18,17 @@ File.open("source/directory", "r") do |f|
     begin
       row << get_relevant_info(url)
     rescue Exception
-
+      row << "na"
     end
 
-    puts row.map{|i| 
-      begin 
-        i.force_encoding(Encoding::UTF_8)
-      rescue Exception
-        ""
-      end
-    }.to_csv
+    rows << row
   end
 end
+
+csv_string = CSV.generate do |csv|
+  rows.each do |row|
+      csv << row
+  end
+end
+
+puts csv_string
